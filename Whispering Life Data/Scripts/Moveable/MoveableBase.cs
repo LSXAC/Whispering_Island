4dd3883e0ce36e510dc4aef19774dfeff3a79249
LSXAC <125983926+LSXAC@users.noreply.github.com>
2025-03-31 15:54:06 +0200

    Minecart start with MoveableBase Class and RailArea to detect, if can be placed on

---
+++ Whispering Life Data/Scripts/Moveable/MoveableBase.cs
@@ -0,0 +1,13 @@
+using System;
+using Godot;
+
+public partial class MoveableBase : Building_Node
+{
+    // Conditions for placement
+    // Physics thinking (Interactions)
+
+    public override void OnMouseClick()
+    {
+        throw new NotImplementedException();
+    }
+}
