19f806cc9272a00d90e9493a67e8e7a3746d630f
LSXAC <125983926+LSXAC@users.noreply.github.com>
2026-02-11 12:03:22 +0100

    Mood & Stability Translations, Auto Translation Update in Monster Island State Panel + Updated Dialogue Box Design

--- Whispering Life Data/UI/Player UI/Moster Island State Panel/MonsterIslandStatePanel.cs
+++ Whispering Life Data/UI/Player UI/Moster Island State Panel/MonsterIslandStatePanel.cs
@@ -18,16 +18,29 @@ public partial class MonsterIslandStatePanel : Panel
     [Export]
     public Array<Texture2D> stability_icons = new Array<Texture2D>();
 
+    public override void _Notification(int what)
+    {
+        if (what != NotificationTranslationChanged)
+            return;
+
+        PlayerUI.instance.monster_island_state_panel.UpdateMoodItem(
+            MonsterIsland.instance.GetCurrentState()
+        );
+        PlayerUI.instance.monster_island_state_panel.UpdateStabiltyItem(
+            MonsterIsland.instance.GetStability()
+        );
+    }
+
     public void UpdateMoodItem(MonsterIslandStateManager.STATE state)
     {
-        state_label.Text = TranslationServer.Translate(state.ToString());
+        state_label.Text = TranslationServer.Translate("MOOD_" + state.ToString());
         state_icon.Texture = state_icons[(int)state];
     }
 
     public void UpdateStabiltyItem(float stability)
     {
         int stability_index = Mathf.RoundToInt(stability * (stability_icons.Count - 1));
-        stability_label.Text = $"Stability: {stability:P0}";
+        stability_label.Text = TranslationServer.Translate("STABILITY") + $": {stability:P0}";
         stability_icon.Texture = stability_icons[stability_index];
     }
 }
